2fe10020153edf39139a75bf50bf3af1f2aa739c
JerryHong <s6323859@hotmail.com>
2016-04-13 21:03:10 +0800

    提高最佳化等級

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var imagemin = require('gulp-imagemin');
 gulp.task('images', function() {
     return gulp.src('./assets/img/**')
         .pipe(imagemin({
-            optimizationLevel: 3,
+            optimizationLevel: 4,
             progressive: true,
             interlaced: true
         }))
